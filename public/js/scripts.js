d5716e8d0d5c1e4f595285207ef61186a7a961d5
MIGUELANGEL GARAY <miguelangelcdm@gmail.com>
2023-05-20 18:28:58 -0500

    mobile_view updated

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -89,32 +89,29 @@ recognition.onresult = function (event) {
   }, 50);
 };
 
-// Delay changing the image source to allow time for the transition effect
-// setTimeout(() => {
-//   // Set opacity to 0 to fade out the current image
-//   image.style.opacity = 0;
-//   image.src = 'img/anim.gif';
-//   // Set opacity to 1 to fade in the new image
-//   image.style.opacity = 1;
-// }, 300);
-
 // Asociar eventos a los botones de inicio y detención
 document.getElementById("start-btn").addEventListener("click", function () {
   recognition.start();
 });
 // const responsev="";
 async function generateResponse(prompt) {
-  // const response = await fetch('/generate', {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //   },
-  //   body: JSON.stringify({ prompt: prompt }),
-  // });
-  // const { answer } = await response.json();
-  // document.getElementById('response').textContent = answer;
-  // responsev=answer;
-  answer =
-    "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum";
+  const startButton = document.getElementById("start-btn");
+  const startIcon = startButton.querySelector("i");
+
+  // Add CSS class to start the tinkling effect
+  startIcon.classList.add("tinkling");
+  const response = await fetch('/generate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ prompt: prompt }),
+  });
+  const { answer } = await response.json();
+  document.getElementById('response').textContent = answer;
   speak(answer);
+  // Remove CSS class to stop the tinkling effect
+  setTimeout(() => {
+    startIcon.classList.remove("tinkling");
+  }, 1000);
 }
